5030fe17813c7c5d56ab49f9d8ad2ec66978819d
Thabang Tlou <thlakott.ys@gmail.com>
2026-09-18 13:22:27 +0200

    Add Firebase packages

---
+++ lib/firebase_options.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return web;
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyCTQEvf6WxIlm5EvmRtlKHqVNzCmFpq1Y',
+    authDomain: 'flutter-auth-demo-2df8c.firebaseapp.com',
+    projectId: 'flutter-auth-demo-2df8c',
+    storageBucket: 'flutter-auth-demo-2df8c.firebasestorage.app',
+    messagingSenderId: '996739692976',
+    appId: '1:996739692976:web:d6a681ef8acf13de88be75',
+  );
+}
